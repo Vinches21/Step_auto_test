3ba5b9ac67052ba1eea65889194293b703c196c0
Vinches21 <Vinches2193@yandex.ru>
2023-01-23 15:22:28 +0300

    Initial commit

---
+++ test_main_page.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
